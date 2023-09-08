7305941d63bea6a01425dbcdd7dcedb41fdc9dae
luaenrique <lua.enriquezan@gmail.com>
2023-09-08 16:39:57 -0300

    feat: cleaning producer

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -85,12 +85,8 @@ class Producer:
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        if self.producer is not None:
+            self.producer.flush()
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
